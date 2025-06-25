76cc08aadb0c382c6dbddfe15e32688e5773cbfd
best <bisongbest04@gmail.com>
2025-06-25 13:48:10 +0100

    commit

--- routes/layout.routes.js
+++ routes/layout.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const layoutController = require('../controllers/layout.controller');
 
 router.post('/create', layoutController.createLayout);
-router.get('/get', layoutController.getLayout);
+router.get('/:Id', layoutController.getLayout);
 
 module.exports = router;
