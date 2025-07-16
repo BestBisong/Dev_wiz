277573d39f786bffb7886d7cd9cbcb3fe0e70b30
BestBisong <157983562+BestBisong@users.noreply.github.com>
2025-07-16 06:04:17 -0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-    const express = require('express');
-    const connectDB = require('./config/db');
-    const cors= require('cors');
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const path = require('path');
 
-    const app = express();
+const app = express();
 
-    app.use(cors({
-        origin: '*',
-        credentials:true
-        }));
+// CORS setup
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 
-    app.use(express.json({ limit: '1GB' })); 
+// JSON Body parser (for large payloads like website layouts)
+app.use(express.json({ limit: '1GB' }));
 
-
-
-    // Initialize database connection
-    connectDB().catch(err => {
+// Initialize database connection
+connectDB().catch(err => {
     console.error('Database connection failed', err);
+});
 
-    });
+// Register routes
+app.use('/layouts', require('./routes/layout.routes')); 
+app.use('/', require('./routes/imageUpload.route'));    // Image upload route with /upload-image and /images
 
 
+app.use('/static', express.static(path.join(__dirname, 'public'))); 
 
-    app.use('/layouts', require('./routes/layout.routes'));
-
-    // Error handling middleware (should be after all other middleware/routes)
-    app.use((err, req, res, next) => {
+// Error handling middleware (should be after all other routes)
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-    });
+});
 
-    // Server setup
-    const PORT = process.env.PORT||2000;
-    app.listen(PORT, () => console.log(`***Application is starting on http://localhost:${PORT}`));
+// Start server
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => console.log(`***Application is starting on http://localhost:${PORT}`));
