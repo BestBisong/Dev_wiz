1102b0beb0ee779901c29cf0cb933ed57109eb23
BestBisong <157983562+BestBisong@users.noreply.github.com>
2025-07-18 13:24:01 +0100

    Update layout.routes.js

--- routes/layout.routes.js
+++ routes/layout.routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const layoutController = require('../controllers/layout.controller');
 
 router.post('/create', layoutController.createLayout);
-router.get('/:Id', layoutController.getLayout);
 
 module.exports = router;
