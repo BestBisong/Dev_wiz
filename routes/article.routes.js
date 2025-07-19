1f2f1380fb2a9efe5110b0a123234e6b49f4d0a9
BestBisong <157983562+BestBisong@users.noreply.github.com>
2025-07-19 06:42:47 -0700

    Update article.routes.js

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const articleController = require('../controllers/article.controller');
 
 // Create new article
-router.post('/', articleController.createArticle);
+router.post('/create', articleController.createArticle);
 
 // Get specific article
 router.get('/:slug', articleController.getArticle);
 
 // Get all published articles
-router.get('/', articleController.getAllArticles);
+router.get('/get', articleController.getAllArticles);
 
 module.exports = router;
