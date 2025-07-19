ab000741164852c01fd27de031f62deaaf673f7b
unknown <bisongbest04.com>
2025-07-19 08:25:28 -0700

    article

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -5,10 +5,4 @@ const articleController = require('../controllers/article.controller');
 // Create new article
 router.post('/create', articleController.createArticle);
 
-// Get specific article
-router.get('/:slug', articleController.getArticle);
-
-// Get all published articles
-router.get('/get', articleController.getAllArticles);
-
 module.exports = router;
